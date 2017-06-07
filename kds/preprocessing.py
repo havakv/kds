9b17fcb20231ffee410d2bbda10c7e9066f2cedc
kvamme_NR <haavard.kvamme@gmail.com>
2017-06-07 21:10:00 +0200

    OneHotSubset didn't work. I think it works now

--- kds/preprocessing.py
+++ kds/preprocessing.py
@@ -4,6 +4,7 @@ Library for preprocessing techniques that are not included elsewhere.
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OneHotEncoder
 
 
 class OneHotSubset(BaseEstimator, TransformerMixin):
@@ -20,39 +21,51 @@ class OneHotSubset(BaseEstimator, TransformerMixin):
     
     ignore_nan: If true we ignore nans, if False we raise an error.
     '''
-    def __init__(self, encode=None, handle_unknown='error', ignore_nan=True):
+    def __init__(self, encode=None, handle_unknown='error', ignore_nan=False):
         self.encode = encode
         self.handle_unknown = handle_unknown
         self.ignore_nan = ignore_nan
     
     def _check_nan(self, x):
         if not self.ignore_nan:
-            if np.isnan(x).any():
+            if x.isnull().any():
                 raise Exception('Array contains nans!')
+        else:
+            raise Exception('This method is currently not able to use np.nan as a separate category,\
+                as we use np.nan to encode all variables that was not fitted.')
     
     def fit(self, x, y=None):
-        self._check_nan(x)
-        self.allVars = self.enc = set(x)
         if hasattr(self.encode, '__iter__'):
-            self.enc = set(self.encode)
+            self.encSet = set(self.encode)
         elif self.encode is not None:
             raise ValueError("Need encode to be iterable or None.")
+        else: 
+            self.encSet = set(x)
+            if self.encSet.difference(set(x)):
+                raise ValueError('The encode list is larger than input x')
+        
+        self.encMap = {k: i+1 for i, k in enumerate(self.encSet)}
+        
+        x = pd.Series(x)
+        self._check_nan(x)
+        xEmb = x.map(self.encMap)
+        xEmb.fillna(0, inplace=True)
+        xfit = xEmb[xEmb != 0].values.reshape(-1, 1)
+        
+        self.encoder = OneHotEncoder(handle_unknown='ignore')
+        self.encoder.fit(xfit)
         return self
         
 
     def transform(self, x):
-        if hasattr(self, 'enc') == False:
+        if hasattr(self, 'encoder') == False:
             raise Error('Need to fit method before we can transform.')
+        if self.handle_unknown not in['error', 'ignore']:
+            raise ValueError('handle_unknown needs to be "error" or "ignore"')
         
-        self._check_nan(x)
-        values = set(x)
-        if self.handle_unknown == 'error' and len(values.difference(self.allVars)) > 0:
-            raise Error('Unknown values!')
-        elif self.handle_unknown not in ['error', 'ignore']:
-            raise ValueError('handle_unknown needs to be "error" or "ignore".')
-            
-        drop = values.difference(self.enc)
-        if len(drop) == 0:
-            return pd.get_dummies(x)
         x = pd.Series(x)
-        return pd.get_dummies(x.replace(list(drop), np.nan))
+        self._check_nan(x)
+        xEmb = x.map(self.encMap)
+        if self.handle_unknown == 'ignore':
+            xEmb.fillna(0, inplace=True)
+        return self.encoder.transform(xEmb.values.reshape(-1, 1))
