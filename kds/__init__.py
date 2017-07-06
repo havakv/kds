08d5d02b2b6f1c3d4b5919b0e05c83f9db538fb2
kvamme_NR <haavard.kvamme@gmail.com>
2017-07-06 10:32:16 +0200

    name change

--- kds/__init__.py
+++ kds/__init__.py
@@ -2,5 +2,7 @@ from . import classification
 from . import keras_help
 from . import tuning
 from . import preprocessing
-from . import teddyb
+from . import teddybear
+
+teddyb = teddybear # Backwards compatibility
 
